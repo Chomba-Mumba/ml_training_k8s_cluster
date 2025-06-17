e0b5d98fcb82548da1f67733e458178033e24f35
chomba-mumba <chombaibex@gmail.com>
2025-06-17 09:18:53 +0100

    removed ecr

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,22 @@
 provider "aws" {
     region = $var.aws_region
 }
+
+terraform {
+  required_version = "~> 1.12"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.100.0"
+    }
+  }
+
+  backend "s3" {
+    bucket = var.ml_tf_backend_bucket
+    key = "./"
+    region = var.aws_region
+    encyrpt = true
+    use_lockfile = true
+  }
+}
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,3 +1,13 @@
 variable "aws_region" {
     type = string
 }
+
+variable "ecr_registry" {
+    type = string
+    default = "${aws_acc}.dkr.ecr.${aws_region}.amazonaws.com"
+}
+
+variable "image_tag" {
+    type = string
+    default = "latest"
+}
