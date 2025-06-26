4c7139c6bf0e9d5fbeb2281af22cfa88997a16ea
chomba-mumba <chombaibex@gmail.com>
2025-06-26 09:32:14 +0100

    WIP: mount efs

--- terraform/efs.tf
+++ terraform/efs.tf
@@ -2,9 +2,11 @@ resource "aws_efs_file_system" "eks_efs" {
     creation_toke = "efs-eks-token"
     perfomance_mode = "generalPurpose"
     throughput_mode = "bursting"
+
     lifecycle_policy {
         transition_to_ia = "AFTER_30_DAYS"
     }
+
     tags = {
         Name = "${var.cluster_name}_efs"
     }
@@ -16,13 +18,10 @@ resource "aws_security_group" "efs_sg" {
     vpc_id = aws_vpc.eks_vpc.id
 
     dynamic "ingress" {
-        for_each = var.efs_sg_ports
-        content {
-            from_port = ingress.value["port"]
-            to_port = ingress.value["port"]
-            protocol = "tcp"
-            security_groups = [aws_security_group.eks_node_sg.id]
-        }
+        from_port = 2049
+        to_port = 2049
+        protocol = "tcp"
+        security_groups = [aws_security_group.eks_node_sg.id]
     }
 
     egress {
@@ -57,4 +56,29 @@ resource "null_resource" "generate_efs_mount_script" {
             "-c"
         ]
     }
+}
+
+resource "aws_iam_policy" "node_efs_policy" {
+    name = "node_efs_policy_${var.cluster_name}"
+    path = "/"
+    description = "Policy for EKS node to access EFS"
+
+    policy = jsonencode({
+        "Statement": [
+            {
+                "Action": [
+                    "elasticfilesystem:DescribeMountTargets",
+                    "elasticfilesystem:DescribeFileSystems",
+                    "elasticfilesystem:DescribeAccessPoints",
+                    "elasticfilesystem:CreateAccessPoint",
+                    "elasticfilesystem:DeleteAccessPoint",
+                    "ec2:DescribeAvailabilityZones"
+                ],
+                "Effect": "Allow",
+                "Resource": "*",
+                "Sid": ""
+            }
+        ],
+        "Version": "2012-10-17"
+    })
 }
