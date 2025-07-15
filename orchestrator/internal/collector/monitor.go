7c2f01e5abb239ae5aa55b20943a6a1a6597ff50
chomba-mumba <chombaibex@gmail.com>
2025-07-15 21:22:38 +0100

    WIP: implement monitor

--- orchestrator/internal/collector/monitor.go
+++ orchestrator/internal/collector/monitor.go
@@ -1,18 +1,86 @@
 package collector
 
+//TODO - stopping criteria: stagnant population/negligable fitness change
+import (
+	"context"
+
+	"github.com/go-gota/gota/dataframe"
+	"github.com/go-gota/gota/series"
+
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
+
+	"log"
+)
+
 type Monitor struct {
-	c       Collector
-	handler func(data interface{})
-	worker  Worker
+	c              Collector
+	handler        func(data interface{})
+	worker         Worker
+	dynamoDBClient *dynamodb.Client
+	tableName      string
+	df             dataframe.DataFrame
+	trainCycle     int
+	patience       int
+}
+
+type tableItem struct {
+	trainCycle      int
+	ID              int
+	fitness         int
+	bestFitness     int //TODO - different average?
+	hyperparameters string
 }
 
 func (m *Monitor) newMonitor() {
+	m.trainCycle = 0
 
-	w := Worker{handler: m.mon_handler, function: "migrator"}
+	// initialise worker
+	src := make(chan message)
+	qt := make(chan struct{})
+	w := Worker{source: src, quit: qt, handler: m.monHandler, function: "migrator"}
 
 	m.worker = w
+
+	//initialise client
+	cfg, err := config.LoadDefaultConfig(context.TODO())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	client := dynamodb.NewFromConfig(cfg)
+	m.dynamoDBClient = client
+
+	df := dataframe.New(
+		series.New(nil, series.Int, "trainCylce"),
+		series.New(nil, series.Int, "island"),
+		series.New(nil, series.Int, "fitness"),
+		series.New(nil, series.Int, "bestFitness"),
+		series.New(nil, series.String, "hyperparameters"),
+	)
+	m.df = df
+
 }
 
-func (m *Monitor) mon_handler(msg message) error {
+func (m *Monitor) addRow(trainCycle int, id int, fit int, best int, hyper int) error {
+	new := dataframe.New(
+		series.New(trainCycle, series.Int, "trainCylce"),
+		series.New(id, series.Int, "island"),
+		series.New(fit, series.Int, "fitness"),
+		series.New(best, series.Int, "bestFitness"),
+		series.New(hyper, series.String, "hyperparameters"),
+	)
+	m.df = m.df.RBind(new)
 	return nil
+}
+
+func (m *Monitor) monHandler(msg message) error {
+	//get most recent fitness
+	cycle := m.trainCycle
+	fil := m.df.Filter(
+		dataframe.F{Colname: "trainCycle", Comparator: series.Eq, Comparando: cycle},
+	)
+
+	m.trainCycle += 1
+
 }
