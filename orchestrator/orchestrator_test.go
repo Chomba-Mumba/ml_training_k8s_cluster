6cdab3ef3d1b845f2f4ab5477b7868209cf43106
chomba-mumba <chombaibex@gmail.com>
2025-07-30 16:44:16 +0100

    changedd to table based testing

--- orchestrator/orchestrator_test.go
+++ orchestrator/orchestrator_test.go
@@ -21,7 +21,8 @@ type migrationPostBody struct {
 }
 
 type mockSQSClient struct {
-	client collector.SQSClientInterface
+	RecMsgFunc func(ctx context.Context, params *sqs.ReceiveMessageInput, optFns ...func(*sqs.Options)) (*sqs.ReceiveMessageOutput, error)
+	DelMsgFunc func(ctx context.Context, params *sqs.DeleteMessageInput, optFns ...func(*sqs.Options)) (*sqs.DeleteMessageOutput, error)
 }
 
 func (m *mockSQSClient) ReceiveMessage(ctx context.Context, params *sqs.ReceiveMessageInput, optFns ...func(*sqs.Options)) (*sqs.ReceiveMessageOutput, error) {
@@ -38,7 +39,7 @@ func (m *mockSQSClient) DeleteMessage(ctx context.Context, params *sqs.DeleteMes
 
 func MockServer() (*httptest.Server, *http.Client) {
 	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		fmt.Println("mock testing server request @: %v", r.URL)
+		fmt.Printf("mock testing server request at: %v", r.URL)
 
 		if r.URL.Path != "/migrant" {
 			fmt.Printf("expected to request '/migrant', got: %s", r.URL.Path)
@@ -66,43 +67,120 @@ func MockServer() (*httptest.Server, *http.Client) {
 
 }
 
+func mockMessage(fitness int, hostname string, hyp map[string]interface{}, han string) collector.Message {
+	msg := collector.Message{}
+	msg.Fitness = fitness
+	msg.Hostname = hostname
+	msg.Hyperparameters = hyp
+	msg.MessageHandle = han
+
+	return msg
+}
+
 func TestMigHandler(t *testing.T) {
 	server, client := MockServer()
 
 	defer server.Close()
 
-	m := collector.Migrator{}
-	m.NewMigrator(client)
-	handle := "handle"
-
-	msg := collector.Message{}
-	msg.Fitness = 32
-	msg.Hostname = "1"
-	msg.Hyperparameters = map[string]interface{}{"h1": "test", "h2": "test"}
-	msg.MessageHandle = &handle
-
-	err := m.MigHandler(msg)
-
-	if err != nil {
-		t.Errorf("expected nil, got an error %v", err)
-	}
-}
-
-func TestNewCollector(t *testing.T) {
-	server, client := MockServer()
-
-	defer server.Close()
 	mig := collector.Migrator{}
 	mig.NewMigrator(client)
 
-	//init monitor
+	var tests = []struct {
+		name string
+		msg  collector.Message
+		want error
+	}{
+		//test cases
+		{
+			"single correct message",
+			collector.Message{
+				Fitness:         4,
+				Hostname:        "1",
+				Hyperparameters: map[string]interface{}{"h1": "test", "h2": "test"},
+				MessageHandle:   "handle",
+			},
+			nil,
+		},
+	}
+
+	//execut subtests
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			resp := mig.MigHandler(tt.msg)
+			if resp != tt.want {
+				t.Errorf("got %s, want %s", resp, tt.want)
+			}
+		})
+	}
+}
+
+func TestMonitorHandler(t *testing.T) {
 	mon := collector.Monitor{}
-	mon.NewMonitor()
+	server, client := MockServer()
 
-	mock := mockSQSClient{}
+	mon.NewMonitor(client)
+	defer server.Close()
 
-	coll := collector.NewCollector(mock.client)
+	var tests = []struct {
+		name string
+		msg  collector.Message
+		want error
+	}{
+		//test cases
+		{
+			"single correct message",
+			collector.Message{
+				Fitness:         4,
+				Hostname:        "1",
+				Hyperparameters: map[string]interface{}{"h1": "test", "h2": "test"},
+				MessageHandle:   "handle",
+			},
+			nil,
+		},
+	}
 
-	workers := []*collector.Worker{mig.Worker, mon.Worker}
-	coll.Collect(workers)
+	//execute subtests
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			resp := mon.MonHandler(tt.msg)
+			if resp != tt.want {
+				t.Errorf("got %s, want %s", resp, tt.want)
+			}
+		})
+	}
+
+}
+
+func TestNewCollector(t *testing.T) {
+	t.Setenv("TOTAL_ISLANDS", "4")
+	t.Setenv("WORKER_POOL", "10")
+	server, client := MockServer()
+
+	defer server.Close()
+	var tests = []struct {
+		name string
+	}{
+		{"correct test"},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			mig := collector.Migrator{}
+			mig.NewMigrator(client)
+
+			//init monitor
+			mon := collector.Monitor{}
+			mon.NewMonitor(client)
+
+			var mockClient collector.SQSAPI = &mockSQSClient{}
+			println("creating new collector")
+			coll := collector.NewCollector(mockClient)
+			println("New Collector has been initalised...")
+
+			workers := []*collector.Worker{mig.Worker, mon.Worker}
+			coll.Collect(workers)
+
+		})
+	}
+
 }
